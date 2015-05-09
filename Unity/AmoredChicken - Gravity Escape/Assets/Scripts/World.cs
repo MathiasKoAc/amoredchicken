025e36aeacf924a8c928b11b026c4711627d3112
Marcel Behrmann <mail@marcelbehrmann.de>
2015-05-09 16:15:39 +0200

    Flipping!

--- Unity/AmoredChicken - Gravity Escape/Assets/Scripts/World.cs
+++ Unity/AmoredChicken - Gravity Escape/Assets/Scripts/World.cs
@@ -10,12 +10,14 @@ public class World : MonoBehaviour {
 
     private Transform _start;
     private ThirdPersonCamera _camera;
+    private GameObject _rotation;
 
     void Start () {
         _start = GameObject.FindGameObjectWithTag("Spawn").transform;
         _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<ThirdPersonCamera>();
 
         _camera.Offset = _start.transform.position - _camera.transform.position;
+        _rotation = GameObject.FindGameObjectWithTag("Rotation");
     }
 	
 	// Update is called once per frame
@@ -24,6 +26,12 @@ public class World : MonoBehaviour {
         {
             _currentPlayer = ((GameObject) Instantiate(Player.gameObject, _start.position, Quaternion.identity)).GetComponent<Player>();
             _camera.LookAt = _currentPlayer.transform;
+            _currentPlayer.Envoriment = this;
         }
 	}
+
+    public void FlipWorld(float direction)
+    {
+        _rotation.transform.RotateAround(_currentPlayer.transform.position, Vector3.forward, direction);
+    }
 }
