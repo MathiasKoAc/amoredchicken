8b77a94dc9e28c7b00e07c89836e1a020871c9bb
Marcel Behrmann <mail@marcelbehrmann.de>
2015-05-10 07:00:50 +0200

    Timing

--- Unity/AmoredChicken - Gravity Escape/Assets/Scripts/Finish.cs
+++ Unity/AmoredChicken - Gravity Escape/Assets/Scripts/Finish.cs
@@ -4,19 +4,22 @@ using System.Collections;
 public class Finish : MonoBehaviour {
 
     public string NextScene;
+    World _manager;
+
 
 	// Use this for initialization
 	void Start () {
-	
+        _manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<World>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
     void OnTriggerEnter()
     {
+        PlayerPrefs.SetInt(Application.loadedLevelName + "_time", _manager.CurrentTime);
         Application.LoadLevel(NextScene);
     }
 }
