435eeef60db8edc304acce5dfe22c9a85f71fe00
Mathias Kohs <mathias.kohs@online.de>
2015-05-10 06:25:42 +0200

    Levels angepasst KillBox und KillerSpoons

---
+++ Unity/AmoredChicken - Gravity Escape/Assets/Scripts/KillerSpoon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillerSpoon : MonoBehaviour {
+
+	World _manager;
+
+	// Use this for initialization
+	void Start () {
+		_manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<World>();
+	}
+	
+	void OnTriggerEnter(Collider c)
+	{
+		if(c.tag == "Player")
+		{
+			_manager.RespawnPlayer();
+		}
+	}
+}
